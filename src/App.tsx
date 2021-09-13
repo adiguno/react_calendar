f19abd7f029c86889200efa6f370bc7c6744186c
adiguno <25011351+adiguno@users.noreply.github.com>
2021-09-13 09:41:09 -0400

    refactor calendar to a react component

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div className="Calendar">
-        <Calendar />
+        <Calendar startDate={0}/>
       </div>
     </div>
   );
--- src/Calendar/Calendar.tsx
+++ src/Calendar/Calendar.tsx
@@ -1,21 +1,35 @@
 import * as React from "react";
 
-export function Calendar() {
-  // let arr = [Array(52).keys()];
+type CalendarState = {
+  startDate: number;
+};
+type CalendarProps = {
+  startDate: number;
+};
 
-  let weeks = [];
-  for (let i = 1; i < 53; i++) {
-    weeks.push(i);
+export class Calendar extends React.Component<CalendarProps, CalendarState> {
+  constructor(props: CalendarProps) {
+    super(props);
+    console.log(props);
   }
+  state: CalendarState = {
+    startDate: 0,
+  };
+  render() {
+    let weeks = [];
+    for (let i = 1; i < 53; i++) {
+      weeks.push(i);
+    }
 
-  var fiftyTwoWeeks = weeks.map((item) => <button>{item}</button>);
+    var fiftyTwoWeeks = weeks.map((item) => <button>{item}</button>);
 
-  let years = [];
-  for (let i = 1; i < 101; i++) {
-    years.push(i);
+    let years = [];
+    for (let i = 1; i < 101; i++) {
+      years.push(i);
+    }
+
+    var hundredYears = years.map((item) => <div>{fiftyTwoWeeks}</div>);
+
+    return <div>{hundredYears}</div>;
   }
-
-  var hundredYears = years.map((item) => <div>{fiftyTwoWeeks}</div>);
-
-  return <div>{hundredYears}</div>;
 }
