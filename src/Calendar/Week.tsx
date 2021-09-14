c1145be8ed4ed014948b6c83ff32ad25c824fb14
adiguno <25011351+adiguno@users.noreply.github.com>
2021-09-14 10:06:47 -0400

    add style for week boxes

--- src/Calendar/Week.tsx
+++ src/Calendar/Week.tsx
@@ -1,16 +1,30 @@
 import React from "react";
+import CSS from "csstype";
 
 type WeekProps = {
-	index: number;
+  index: number;
 };
 
 export class Week extends React.Component<WeekProps, {}> {
-	constructor(props: WeekProps) {
-		super(props);
-	}
+  constructor(props: WeekProps) {
+    super(props);
+  }
 
-	render() {
-        // return(<button>{this.props.index}</button>)
-		return <input type="radio" />;
-	}
+  render() {
+    return (
+      <div style={this.style}>
+        {/* <label>{this.props.index}</label> */}
+        <label></label>
+      </div>
+    );
+  }
+
+  style: CSS.Properties = {
+    height: "20px",
+    width: "20px",
+    padding: "1px",
+    margin: "2px",
+    backgroundColor: "rgba(77, 138, 235, 0.85)",
+    borderRadius: "5px",
+  };
 }
--- src/Calendar/Year.tsx
+++ src/Calendar/Year.tsx
@@ -1,26 +1,32 @@
 import React from "react";
 import { Week } from "./Week";
+import CSS from "csstype";
 
 type YearProps = {
-	index: number;
+  index: number;
 };
 
 export class Year extends React.Component<YearProps, {}> {
-	constructor(props: YearProps) {
-		super(props);
-	}
+  constructor(props: YearProps) {
+    super(props);
+  }
 
-	render() {
-		return <div>{this.renderWeeks(52)}</div>;
-	}
+  render() {
+    return <div style={this.style}>{this.renderWeeks(52)}</div>;
+  }
 
-	renderWeeks(num: number) {
-		let weeks = [];
-		let week;
-		for (let i = 1; i < num + 1; i++) {
-			week = <Week key={`week${i}`} index={i} />;
-			weeks.push(week);
-		}
-		return weeks;
-	}
+  renderWeeks(num: number) {
+    let weeks = [];
+    let week;
+    for (let i = 1; i < num + 1; i++) {
+      week = <Week key={`week${i}`} index={i} />;
+      weeks.push(week);
+    }
+    return weeks;
+  }
+
+  style: CSS.Properties = {
+    display: "flex",
+    flexDirection: "row",
+  };
 }
