222c8813db59ce6ed62ac51eaa160bab98cf6b00
adiguno <25011351+adiguno@users.noreply.github.com>
2021-09-14 09:14:01 -0400

    css and prettierrc'

--- src/Calendar/Week.tsx
+++ src/Calendar/Week.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { NONAME } from "dns";
+import React, { CSSProperties } from "react";
 
 type WeekProps = {
 	index: number;
@@ -10,7 +11,25 @@ export class Week extends React.Component<WeekProps, {}> {
 	}
 
 	render() {
-        // return(<button>{this.props.index}</button>)
+		return (
+			<label style={this.labelStyle}>
+				<input type="radio" className="demo" style={this.inputStyle} />
+				<span style={this.spanStyle}>a</span>
+			</label>
+		);
+		// return(<button>{this.props.index}</button>)
 		return <input type="radio" />;
 	}
+
+	inputStyle: CSSProperties = {
+		display: "none",
+	};
+	labelStyle: CSSProperties = {
+		display: "inline-block",
+		padding: "5px 10px",
+		cursor: "pointer",
+	};
+	spanStyle: CSSProperties = {
+		position: "relative",
+	};
 }
